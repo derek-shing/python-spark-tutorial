78a1fb8b0fb4ddec99d13fa2e24e363ddf59c7be
derek-shing <hpshing@gmail.com>
2019-09-12 00:28:47 -0700

    add hand on big data example

--- rdd/WordCount.py
+++ rdd/WordCount.py
@@ -4,12 +4,12 @@ if __name__ == "__main__":
     conf = SparkConf().setAppName("word count").setMaster("local[3]")
     sc = SparkContext(conf = conf)
     
-    lines = sc.textFile("in/word_count.text")
+    #lines = sc.textFile("in/word_count.text")
     
-    words = lines.flatMap(lambda line: line.split(" "))
+    #words = lines.flatMap(lambda line: line.split(" "))
     
-    wordCounts = words.countByValue()
+    #wordCounts = words.countByValue()
     
-    for word, count in wordCounts.items():
-        print("{} : {}".format(word, count))
+    #for word, count in wordCounts.items():
+    #    print("{} : {}".format(word, count))
 
